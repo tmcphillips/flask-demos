fd8a16f90d2a455aeb1b739496ccaf3e4e4d52c2
Timothy McPhillips <tmcphillips@absoluteflow.org>
2018-08-07 18:32:46 -0700

    minimal JSON-returning hello world REST service using flask

--- flask-rest-api/app.py
+++ flask-rest-api/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return jsonify({'greeting': 'Hello World!'})
 
 app.run(port=5000)

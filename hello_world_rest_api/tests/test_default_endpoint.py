0cb5401acdf47420b27ef6ea18dc5769cef9afa1
Timothy McPhillips <tmcphillips@absoluteflow.org>
2018-08-16 17:13:38 -0700

    made test fixture module scope

--- hello_world_rest_api/tests/test_default_endpoint.py
+++ hello_world_rest_api/tests/test_default_endpoint.py
@@ -2,17 +2,10 @@ import pytest
 
 from hello_world_rest_api.app import app
 
-@pytest.fixture
-def client(request):
-    return app.test_client()
-
-@pytest.fixture
-def response(client):
+@pytest.fixture(scope='module')
+def response():
+    client = app.test_client()
     return client.get('/')
-
-@pytest.fixture
-def response_json(response):
-    return response.get_json()
 
 def test_response_status_code_is_200(response):
     assert response.status_code == 200
@@ -32,23 +25,23 @@ def test_response_header_has_two_keys(response):
 def test_response_header_has_key_content_type(response):
     assert 'Content-Type' in response.headers
 
+def test_response_header_has_key_content_length(response):
+    assert 'Content-Length' in response.headers
+
 def test_response_content_type_is_application_json(response):
     assert response.headers['Content-Type'] == 'application/json'
 
 def test_response_content_length_is_greater_than_zero(response):
     assert int(response.headers['Content-Length']) > 0
 
-def test_response_header_has_key_content_length(response):
-    assert 'Content-Length' in response.headers
-
-def test_response_contains_hello_world_message(response):
+def test_response_data_contains_hello_world_message(response):
     assert b'Hello World!' in response.data
 
-def test_response_json_contains_one_key(response_json):
-    assert len(response_json) == 1
+def test_response_json_contains_one_property(response):
+    assert len(response.get_json()) == 1
 
-def test_response_json_has_key_greeting(response_json):
-    assert 'greeting' in response_json
+def test_response_json_has_key_greeting(response):
+    assert 'greeting' in response.get_json()
 
-def test_value_of_response_json_greeting_property_is_hello_world(response_json):
-    assert response_json['greeting'] == 'Hello World!'
+def test_response_json_greeting_property_is_hello_world(response):
+    assert response.get_json()['greeting'] == 'Hello World!'
